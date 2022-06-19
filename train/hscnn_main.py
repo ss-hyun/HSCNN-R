3504296fdd8f767f93947382f0c139c1214bebe8
ss-hyun <yunatlsghk6@ajou.ac.kr>
2022-06-20 04:51:39 +0900

    Complete initial setting

--- train/hscnn_main.py
+++ train/hscnn_main.py
@@ -7,44 +7,48 @@ import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 
-import  os
+import os
 import time
 import scipy.io as sio
 
 from dataset import DatasetFromHdf5
-from resblock import resblock,conv_relu_res_relu_block
-from utils import AverageMeter,initialize_logger,save_checkpoint,record_loss
+from resblock import resblock, conv_relu_res_relu_block
+from utils import AverageMeter, initialize_logger, save_checkpoint, record_loss
 from loss import rrmse_loss
 
+
 def main():
-    
     cudnn.benchmark = True
-    
+
     # Dataset
-    train_data = DatasetFromHdf5('./train.h5')
+    train_data = DatasetFromHdf5('../data/hdf5_data/train_test.h5')
     print(len(train_data))
-    val_data = DatasetFromHdf5('./test.h5')
+    val_data = DatasetFromHdf5('../data/hdf5_data/valid_test.h5')
     print(len(val_data))
+    per_iter_time = len(train_data)
 
     # Data Loader (Input Pipeline)
-    train_data_loader = DataLoader(dataset=train_data, 
-                                   num_workers=1,  
+    train_data_loader = DataLoader(dataset=train_data,
+                                   num_workers=3,
                                    batch_size=64,
                                    shuffle=True,
                                    pin_memory=True)
     val_loader = DataLoader(dataset=val_data,
-                            num_workers=1, 
+                            num_workers=0,
                             batch_size=1,
                             shuffle=False,
-                           pin_memory=True)
+                            pin_memory=True)
 
-    # Model               
-    model = resblock(conv_relu_res_relu_block, 16, 3, 31)
+    # Model
+    layer = 14
+    input_channel = 30
+    output_channel = 100
+    model = resblock(conv_relu_res_relu_block, layer, input_channel, output_channel)
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(model)
     if torch.cuda.is_available():
         model.cuda()
-  
+
     # Parameters, Loss and Optimizer
     start_epoch = 0
     end_epoch = 1000
@@ -52,18 +56,18 @@ def main():
     iteration = 0
     record_test_loss = 1000
     criterion = rrmse_loss
-    optimizer=torch.optim.Adam(model.parameters(), lr=init_lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
-    
+    optimizer = torch.optim.Adam(model.parameters(), lr=init_lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+
     model_path = './models/'
     if not os.path.exists(model_path):
         os.makedirs(model_path)
-    loss_csv = open(os.path.join(model_path,'loss.csv'), 'w+')
-    
-    log_dir = os.path.join(model_path,'train.log')
+    loss_csv = open(os.path.join(model_path, 'loss.csv'), 'w+')
+
+    log_dir = os.path.join(model_path, 'train.log')
     logger = initialize_logger(log_dir)
-    
+
     # Resume
-    resume_file = '' 
+    resume_file = ''
     if resume_file:
         if os.path.isfile(resume_file):
             print("=> loading checkpoint '{}'".format(resume_file))
@@ -72,77 +76,82 @@ def main():
             iteration = checkpoint['iter']
             model.load_state_dict(checkpoint['state_dict'])
             optimizer.load_state_dict(checkpoint['optimizer'])
-       
-    for epoch in range(start_epoch+1, end_epoch):
-        
-        start_time = time.time()         
-        train_loss, iteration, lr = train(train_data_loader, model, criterion, optimizer, iteration, init_lr, end_epoch)
+
+    for epoch in range(start_epoch + 1, end_epoch):
+
+        start_time = time.time()
+        train_loss, iteration, lr = train(train_data_loader, model, criterion, optimizer, iteration, init_lr
+                                          , end_epoch, max_iter=per_iter_time * end_epoch, power=1.5)
         test_loss = validate(val_loader, model, criterion)
-        
- 
-        
+
         # Save model
         if test_loss < record_test_loss:
             record_test_loss = test_loss
-            save_checkpoint(model_path, epoch, iteration, model, optimizer)
-        
+            save_checkpoint(model_path, epoch, iteration, model, optimizer, layer, input_channel, output_channel)
+
         # print loss 
         end_time = time.time()
         epoch_time = end_time - start_time
-        print ("Epoch [%d], Iter[%d], Time:%.9f, learning rate : %.9f, Train Loss: %.9f Test Loss: %.9f " %(epoch, iteration, epoch_time, lr, train_loss, test_loss))
+        print("Epoch [%d], Iter[%d], Time:%.9f, Train Loss: %.9f Test Loss: %.9f, learning rate:"
+              % (epoch, iteration, epoch_time, train_loss, test_loss), lr)
+
         # save loss
-        record_loss(loss_csv,epoch, iteration, epoch_time, lr, train_loss, test_loss)     
-        logger.info("Epoch [%d], Iter[%d], Time:%.9f, learning rate : %.9f, Train Loss: %.9f Test Loss: %.9f " %(epoch, iteration, epoch_time, lr, train_loss, test_loss))
-    
+        record_loss(loss_csv, epoch, iteration, epoch_time, lr, train_loss, test_loss)
+        logger.info("Epoch [%d], Iter[%d], Time:%.9f, Train Loss: %.9f Test Loss: %.9f, learning rate:"
+                    % (epoch, iteration, epoch_time, train_loss, test_loss) + ' {}'.format(lr))
+
+
 # Training 
-def train(train_data_loader, model, criterion, optimizer, iteration, init_lr ,end_epoch):
+def train(train_data_loader, model, criterion, optimizer, iteration, init_lr, end_epoch, max_iter=1e8, power=1.5):
     losses = AverageMeter()
-    for i, (images, labels) in enumerate(train_data_loader):
-        labels = labels.cuda(async=True)
-        images = images.cuda(async=True)
+    for i, (images, labels) in enumerate(train_data_loader.dataset):
+        labels = labels.cuda(non_blocking=True)
+        images = images.cuda(non_blocking=True)
         images = Variable(images)
-        labels = Variable(labels)    
-        
+        labels = Variable(labels)
+
         # Decaying Learning Rate
-        
-        lr = poly_lr_scheduler(optimizer, init_lr, iteration, max_iter=968000, power=1.5) 
+
+        lr = poly_lr_scheduler(optimizer, init_lr, iteration, max_iter=max_iter, power=power)
         iteration = iteration + 1
+
         # Forward + Backward + Optimize       
         output = model(images)
-        
+
         loss = criterion(output, labels)
         optimizer.zero_grad()
         loss.backward()
-        
+
         # Calling the step function on an Optimizer makes an update to its parameters
         optimizer.step()
-        
+
         #  record loss
-        losses.update(loss.data[0])
-            
+        losses.update(loss.data)
+
     return losses.avg, iteration, lr
+
 
 # Validate
 def validate(val_loader, model, criterion):
-    
-    
     model.eval()
     losses = AverageMeter()
 
     for i, (input, target) in enumerate(val_loader):
-        input = input.cuda(async=True)
-        target = target.cuda(async=True)
-        input_var = torch.autograd.Variable(input, volatile=True)
-        target_var = torch.autograd.Variable(target, volatile=True)
+        with torch.no_grad():
+            input = input.cuda(non_blocking=True)
+            target = target.cuda(non_blocking=True)
+            input_var = Variable(input)
+            target_var = Variable(target)
 
-        # compute output
-        output = model(input_var)      
-        loss = criterion(output, target_var)
+            # compute output
+            output = model(input_var)
+            loss = criterion(output, target_var)
 
-        #  record loss
-        losses.update(loss.data[0])
+            #  record loss
+            losses.update(loss.data)
 
     return losses.avg
+
 
 # Learning rate
 def poly_lr_scheduler(optimizer, init_lr, iteraion, lr_decay_iter=1,
@@ -158,11 +167,12 @@ def poly_lr_scheduler(optimizer, init_lr, iteraion, lr_decay_iter=1,
     if iteraion % lr_decay_iter or iteraion > max_iter:
         return optimizer
 
-    lr = init_lr*(1 - iteraion/max_iter)**power
+    lr = init_lr * (1 - iteraion / max_iter) ** power
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
     return lr
+
 
 if __name__ == '__main__':
     main()
--- train/loss.py
+++ train/loss.py
@@ -6,24 +6,35 @@ import random
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
+import numpy as np
+
 
 class reconstruct_loss(nn.Module):
     """the loss between the input and synthesized input"""
+
     def __init__(self, cie_matrix, batchsize):
         super(reconstruct_loss, self).__init__()
         self.cie = Variable(torch.from_numpy(cie_matrix).float().cuda(), requires_grad=False)
         self.batchsize = batchsize
+
     def forward(self, network_input, network_output):
         network_output = network_output.permute(3, 2, 0, 1)
         network_output = network_output.contiguous().view(-1, 31)
-        reconsturct_input = torch.mm(network_output,self.cie)
+        reconsturct_input = torch.mm(network_output, self.cie)
         reconsturct_input = reconsturct_input.view(50, 50, 64, 3)
-        reconsturct_input = reconsturct_input.permute(2,3,1,0)
-        reconstruction_loss = torch.mean(torch.abs(reconsturct_input - network_input))                  
+        reconsturct_input = reconsturct_input.permute(2, 3, 1, 0)
+        reconstruction_loss = torch.mean(torch.abs(reconsturct_input - network_input))
         return reconstruction_loss
+
 
 def rrmse_loss(outputs, label):
     """Computes the rrmse value"""
-    error = torch.abs(outputs-label)/label
+    error = torch.abs(outputs - label) / label
+    # print(label, error)
+    # print(label.shape)
+    # labeltest = label.cpu()
+    # if np.shape(labeltest[np.where(labeltest == 0)])[0] != 0:
+    #     print("error")
+    #     exit()
     rrmse = torch.mean(error.view(-1))
     return rrmse
