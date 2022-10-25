a6090a2198a165ef5ac3de515f1a6939254ed771
sshyun <yunatlsghk6@ajou.ac.kr>
2022-10-25 22:45:51 +0900

    Make new loss function - sam + rrmse

--- train/loss.py
+++ train/loss.py
@@ -31,107 +31,29 @@ class reconstruct_loss(nn.Module):
         return reconstruction_loss
 
 
+def loss(outputs, label):
+    if outputs.dim() == 4:
+        outputs = outputs.squeeze()
+        label = label.squeeze()
+
+    reflect_t = torch.transpose(outputs.view([label.shape[0], -1]), 0, 1)
+    reflect_r = torch.transpose(label.view([label.shape[0], -1]), 0, 1)
+    sam = torch.arccos((reflect_r * reflect_t).sum(dim=1)
+                       / torch.sqrt((reflect_t ** 2).sum(dim=1) * (reflect_r ** 2).sum(dim=1) + 1e-9))
+    # check = torch.isnan(sam)
+    # if check[check == True].size(0):
+    #     print(reflect_r[check == True], reflect_t[check == True])
+
+    error = torch.abs(outputs - label) / label
+
+    return torch.mean(sam) + torch.mean(error.view(-1))
+
+
 def rrmse_loss(outputs, label):
     """Computes the rrmse value"""
     if outputs.dim() == 4:
         outputs = outputs.squeeze()
         label = label.squeeze()
     error = torch.abs(outputs - label) / label
-    # rrmse = torch.mean(error.view(-1))
-
-    # if rrmse < 0.002:
-    chan = label.shape[0]
-    # flat_error = error.clone().detach().view([chan, -1])
-    # flat_outputs = outputs.clone().detach().view([chan, -1])
-    # flat_label = label.clone().detach().view([chan, -1])
-
-    flat_error = torch.zeros_like(error)
-    flat_outputs = torch.zeros_like(outputs)
-    flat_label = torch.zeros_like(label)
-
-    for i in range(chan - 1):
-        # lf = i - 1 if i > 0 else 0
-        r = i + 1 if i < chan - 1 else chan - 1
-
-        flat_error[i] = outputs[i] * label[i]
-        flat_outputs[i] = outputs[i] ** 2
-        flat_label[i] = label[i] ** 2
-
-        # y1 = lambda a, b: (flat_outputs[a] - flat_outputs[b])
-        # y2 = lambda a, b: (flat_label[a] - flat_label[b])
-
-        # val = (
-        #         torch.abs(torch.arccos(torch.round(
-        #             torch.sqrt(((flat_rrmse ** 2 + y1(r, i) * y2(r, i)) ** 2)
-        #                        / (flat_rrmse ** 2 + y1(r, i) ** 2) / (flat_rrmse ** 2 + y2(r, i) ** 2))
-        #             , decimals=3)))
-        #         +
-        #         torch.abs(torch.arccos(torch.round(
-        #             torch.sqrt(((flat_rrmse ** 2 + y1(i, lf) * y2(i, lf)) ** 2)
-        #                        / (flat_rrmse ** 2 + y1(i, lf) ** 2) / (flat_rrmse ** 2 + y2(i, lf) ** 2))
-        #             , decimals=3)))
-        # )
-        # check = torch.isnan(val)
-        # if check[check==True].size(0):
-        #     temp = torch.sqrt(((flat_rrmse ** 2 + y1(r, i) * y2(r, i)) ** 2)
-        #                       / (flat_rrmse ** 2 + y1(r, i) ** 2) / (flat_rrmse ** 2 + y2(r, i) ** 2))
-        #     for j, nan in enumerate(temp):
-        #         if nan > 1 or nan < -1:
-        #             print("1: ", i, j, "%.20f" % temp[j], (flat_rrmse ** 2 + y1(r, i) * y2(r, i))[j]
-        #                   , torch.sqrt((flat_rrmse ** 2 + y1(r, i) ** 2) * (flat_rrmse ** 2 + y2(r, i) ** 2))[j]
-        #                   , y1(r, i)[j], y2(r, i)[j], flat_rrmse)
-        #     temp = torch.sqrt(((flat_rrmse ** 2 + y1(i, lf) * y2(i, lf)) ** 2)
-        #                       / (flat_rrmse ** 2 + y1(i, lf) ** 2) / (flat_rrmse ** 2 + y2(i, lf) ** 2))
-        #     for j, nan in enumerate(temp):
-        #         if nan > 1 or nan < -1:
-        #             print("2: ", i, j, "%.20f" % temp[j], (flat_rrmse ** 2 + y1(i, lf) * y2(i, lf))[j]
-        #                   , torch.sqrt((flat_rrmse ** 2 + y1(i, lf) ** 2) * (flat_rrmse ** 2 + y2(i, lf) ** 2))[j]
-        #                   , y1(i, lf)[j], y2(i, lf)[j], flat_rrmse)
-
-        # flat_error[i] = (
-        # torch.arccos(torch.round(
-        #     torch.sqrt(((flat_rrmse ** 2 + y1(r, i) * y2(r, i)) ** 2)
-        #                / (flat_rrmse ** 2 + y1(r, i) ** 2) / (flat_rrmse ** 2 + y2(r, i) ** 2))
-        #     , decimals=3))
-        # +
-        # torch.arccos(torch.round(
-        #     torch.sqrt(((flat_rrmse ** 2 + y1(i, lf) * y2(i, lf)) ** 2)
-        #                / (flat_rrmse ** 2 + y1(i, lf) ** 2) / (flat_rrmse ** 2 + y2(i, lf) ** 2))
-        #     , decimals=3))
-        # )
-
-    flat_error = torch.sum(flat_error, dim=0)
-    flat_outputs = torch.sum(flat_outputs, dim=0)
-    flat_label = torch.sum(flat_label, dim=0)
-
-    # t=torch.sqrt(flat_outputs * flat_label)
-    # t = torch.arccos(torch.round(flat_error/torch.sqrt(flat_outputs*flat_label), decimals=4))
-    # check = torch.isnan(t)
-    # if check[check==True].size(0):
-    #     for i, nan in enumerate(t):
-    #         if torch.isnan(nan):
-    #             print(flat_error[i], flat_outputs[i], flat_label[i], flat_error[i]/torch.sqrt(flat_outputs[i]*flat_label[i]))
-    #     exit()
-    #     print(rrmse, t)
-
-    flat_outputs[flat_outputs < 0] = 0
-    flat_error = flat_error / torch.sqrt(flat_outputs * flat_label)
-    flat_error[flat_error > 1] = 0
-    check = torch.isnan(flat_error)
-    # for i, fe in enumerate(flat_error):
-    #     for j, nan in enumerate(fe):
-    #         print(nan)
-    #         print(torch.isnan(nan))
-    #         exit()
-    if check[check==True].size(0):
-        for i, fe in enumerate(flat_error):
-            for j, nan in enumerate(fe):
-                if torch.isnan(nan):
-                    print(flat_error[i][j], flat_outputs[i][j], flat_label[i][j], flat_error[i][j]/torch.sqrt(flat_outputs[i][j]*flat_label[i][j]))
-        exit()
-
-    sam = Variable(torch.mean(torch.arccos(flat_error)), requires_grad=True)
-
-    return sam
-
-    # return rrmse
+    rrmse = torch.mean(error.view(-1))
+    return rrmse
